b71ab5dee42f6673e5dd6274172c2637ba53f71e
ActuallyAJL <zero3260@hotmail.com>
2022-03-14 15:39:12 -0500

    added a date chooser box

--- scripts/main.js
+++ scripts/main.js
@@ -2,15 +2,22 @@ const picOfDay = document.querySelector("#pic-of-day");
 const picBTN = document.querySelector("#pic-button");
 const APIKey = "xughmG259eXvPiKheeSAVhT2eKh0cJ6Vh8NWAwIV";
 const APILink = "https://api.nasa.gov/planetary/apod?api_key=xughmG259eXvPiKheeSAVhT2eKh0cJ6Vh8NWAwIV";
+const dateSelected = document.querySelector("#date-el");
 
-const getAPIPicture = () => {
-    return fetch(APILink)
+const getAPIPicture = (thisLink) => {
+    return fetch(thisLink)
     .then(response => response.json())
     .then(spaceImage => spaceImage)
 }
 
 const getAPicture = () => {
-    getAPIPicture().then((object) =>{
+    let tempLink = "";
+    if (dateSelected.value) {
+        tempLink = APILink + `&date=${dateSelected.value}`;
+    } else {
+        tempLink = APILink;
+    }
+    getAPIPicture(tempLink).then((object) =>{
         picOfDay.innerHTML += `
         <img src='${object.hdurl}' alt='${object.explanation}'/>
         <p>${object.explanation}</p>
