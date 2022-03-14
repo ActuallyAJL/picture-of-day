c5a3217420a6605cc074508dd0928900f31bb5e3
ActuallyAJL <zero3260@hotmail.com>
2022-03-14 14:42:26 -0500

    completed project

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,21 @@
+const picOfDay = document.querySelector("#pic-of-day");
+const picBTN = document.querySelector("#pic-button");
+const APIKey = "xughmG259eXvPiKheeSAVhT2eKh0cJ6Vh8NWAwIV";
+const APILink = "https://api.nasa.gov/planetary/apod?api_key=xughmG259eXvPiKheeSAVhT2eKh0cJ6Vh8NWAwIV";
 
+const getAPIPicture = () => {
+    return fetch(APILink)
+    .then(response => response.json())
+    .then(spaceImage => spaceImage)
+}
+
+const getAPicture = () => {
+    getAPIPicture().then((object) =>{
+        picOfDay.innerHTML += `
+        <img src='${object.hdurl}' alt='${object.explanation}'/>
+        <p>${object.explanation}</p>
+        ` 
+    })
+}
+
+picBTN.addEventListener("click" , getAPicture);
